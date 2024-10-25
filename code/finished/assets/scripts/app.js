6cbebd745bae6a7f51438d1816d12022fee65002
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-10-25 20:07:35 +0800

    05-arrow-functions

--- code/finished/assets/scripts/app.js
+++ code/finished/assets/scripts/app.js
@@ -1,11 +1,4 @@
-function createGreeting(userName, message = "Hello!") {
-    console.log(userName);
-    console.log(message);
-    return "Hi, I am " + userName + ". " + message;
-}
-
-const greeting1 = createGreeting("Max");
-console.log(greeting1);
-
-const greeting2 = createGreeting("Manuel", "Hello, what's up?");
-console.log(greeting2);
+export default (userName, message) => {
+    console.log("Hello");
+    return userName + message;
+};
