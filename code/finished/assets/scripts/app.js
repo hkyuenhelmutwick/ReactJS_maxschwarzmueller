00a33b0c7cf812c4c96a4189ab7804f00c186701
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-10-25 19:59:36 +0800

    02-variables-values

--- code/finished/assets/scripts/app.js
+++ code/finished/assets/scripts/app.js
@@ -1,8 +1,4 @@
-// import { apiKey } from "./util.js";
+// const userMessage = "Hello World!!!";
+let userMessage = "Hello World!!!";
 
-// import apiKey from "./util.js";
-import { apiKey, abc as content } from "./util.js";
-// import * as util from "./util.js";
-
-// console.log(util.apiKey);
-console.log(content);
+console.log(userMessage);
