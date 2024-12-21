a6be3eec736dd4e5acfec1abef0f1c005a080912
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-12-21 10:35:15 +0800

    08-debouncing

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function SearchableList({ items, itemKeyFn, children }) {
+  const lastChange = useRef();
   const [searchTerm, setSearchTerm] = useState('');
 
   const searchResults = items.filter((item) =>
@@ -8,7 +9,14 @@ export default function SearchableList({ items, itemKeyFn, children }) {
   );
 
   function handleChange(event) {
-    setSearchTerm(event.target.value);
+    if (lastChange.current) {
+      clearTimeout(lastChange.current)
+    }
+
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null
+      setSearchTerm(event.target.value);
+    }, 500);
   }
 
   return (
@@ -16,9 +24,7 @@ export default function SearchableList({ items, itemKeyFn, children }) {
       <input type="search" placeholder="Search" onChange={handleChange} />
       <ul>
         {searchResults.map((item) => (
-          <li key={itemKeyFn(item)}>
-            {children(item)}
-          </li>
+          <li key={itemKeyFn(item)}>{children(item)}</li>
         ))}
       </ul>
     </div>
